c0db69c9c741246c9c57f4c3f2713e50aab424ba
poornithsundar <40591754+poornithsundar@users.noreply.github.com>
2020-06-24 14:31:22 +0530

    RBI Official

--- src/check.java
+++ src/check.java
@@ -2,69 +2,68 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.HashMap;
 import java.util.Scanner;
+import java.io.FileWriter;
 
 public class check {
 	public static void main(String[] args) throws Exception
 	{
+		String location = "RBI_Ifsc.xls";
+		FileWriter writer = new FileWriter(location);
+		writer.append("Bank Name");
+	    writer.append('\t');
+	    writer.append("Address");
+	    writer.append('\t');
+	    writer.append("IFSC");
+	    writer.append('\n');
 		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter IFSC code:");
-		String ifs = sc.next();
-		String url = "https://ifsc-finder.com/IFSC/"+ifs;
+		for(int i=1;i<=154734;i++)
+		{
+		String url = "https://www.rbi.org.in/Scripts/IFSCDetails.aspx?pkid="+i;
 	    URL obj = new URL(url);
 	    HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-	    // optional default is GET
+	    System.setProperty("https.proxyHost", "myProxy");
+	    System.setProperty("https.proxyPort", "80");
 	    con.setRequestMethod("GET");
-	    //add request header
 	    con.setRequestProperty("User-Agent", "Mozilla/5.0");
 	    con.setRequestProperty("Accept-Charset", "ISO-8859-1"); 
-	    int responseCode = con.getResponseCode();
-	    System.out.println("\nSending 'GET' request to URL : " + url);
-	    System.out.println("Response Code : " + responseCode);
 	    BufferedReader in = new BufferedReader(
 	            new InputStreamReader(con.getInputStream()));
 	    String inputLine;
-	    HashMap<String,String> ifsc = new HashMap<>();
-	    String temp="";
-	    int i=0;
 	    sc.close();
-	    boolean flag = false;
-	    while ((inputLine = in.readLine()) != null) {
-	    	if(inputLine.contains("/tbody"))
+	    int flag = 0,ii=0;
+	    String temp = "";
+	    while ((inputLine = in.readLine()) != null) 
+	    {
+	    	if(inputLine.contains("pnlDetails"))
 	    	{
-	    		flag = false;
+	    		flag = 1;
 	    	}
-	    	else if(inputLine.contains("tbody"))
+	    	else if(inputLine.contains("/div")) 
 	    	{
-	    		flag = true;
+	    		flag = 0;
 	    	}
-	    	if(flag)
+	    	if(flag==1)
 	    	{
-	    		inputLine = inputLine.replaceAll("\\<.*?\\>", "");
-	    		if(inputLine.length()!=0 && !inputLine.contains("Click here for all the branches"))
+	    		inputLine = inputLine.replaceAll("\\<.*?\\>", "").trim();
+	    		if(inputLine.length()!=0)
 	    		{
-		    		temp+=inputLine;
-		    		i++;
-		    		if(i==1) temp+="?";
+		    		temp+= (ii!=1)?(inputLine)+"?":(inputLine);
+		    		ii++;
 		    	}
 	    	}
-	    	if(i==2)
+	    	if(ii==2)
 	    	{
-	    		ifsc.put(temp.substring(0, temp.indexOf("?")),temp.substring(temp.indexOf("?")+1));
+	    		writer.append(temp.substring(temp.indexOf("?")+1,temp.length()));
+		   	    writer.append('\t');
 	    		temp="";
-	    		i=0;
-	    	}
+	    		ii=0;
+	    	}	
 	    }
-	    in.close();
-	    ifsc.remove("CITY , DISTRICT , STATE");
-	    if(ifsc.size()==0)
-	    {
-	    	System.out.println("Wrong IFSC Code");
-	    }
-	    else
-	    {
-	    	System.out.println(ifsc.toString());
-	    }
+	    writer.append('\n');
+	    System.out.println(i);
+		}
+		writer.flush();
+	    writer.close();
 	}
 }
